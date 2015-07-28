7626117656072d9b26162bef8fb98e238d0b9e30
Sylvestre <msylvestreets@yahoo.fr>
2015-07-28 22:46:07 +0200

    Revert "Init web-service"
    This reverts commit 7a4f9e63ed4f3da4b3b29b327aa82c3035435df1.

--- Web-service/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	ob_start();
-	$_config['DB_host'] = 'localhost';
-	$_config['DB_user'] = 'root';
-	$_config['DB_pass'] = '';
-	$_config['DB_name'] = 'Hours';
-	$_config['DB_prefix'] = 'hours_';
-?>
