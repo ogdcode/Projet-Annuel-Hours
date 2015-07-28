7a4f9e63ed4f3da4b3b29b327aa82c3035435df1
Sylvestre <msylvestreets@yahoo.fr>
2015-07-28 22:45:50 +0200

    Init web-service
    Mise en place des web-service

---
+++ Web-service/config.php
@@ -0,0 +1,8 @@
+<?php
+	ob_start();
+	$_config['DB_host'] = 'localhost';
+	$_config['DB_user'] = 'root';
+	$_config['DB_pass'] = '';
+	$_config['DB_name'] = 'Hours';
+	$_config['DB_prefix'] = 'hours_';
+?>
